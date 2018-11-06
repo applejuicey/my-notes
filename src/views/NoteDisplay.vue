c38e649f545e308c3573b22e2fdf35acb5afb0df
yangfan <yangfan_94@hotmail.com>
2018-11-06 18:07:03 +0800

    element-ui added; root path revised; NoteDisplay page added; 404 page added.

---
+++ src/views/NoteDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="note-display">
+    <h1>This is a note display page</h1>
+    <div v-if="checkNoteID">
+      you are reading note: id={{ $route.params.noteid}}
+    </div>
+    <div v-else>
+      you have entered an error ID
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NoteDisplay',
+    computed : {
+      checkNoteID: function () {
+        if (isNaN(this.$route.params.noteid)) {
+          // 传入的noteid不是数字
+          return false;
+        } else {
+          // 传入的noteid没问题
+          return true;
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Page404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page404">
+    <h1>i am page 404</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
